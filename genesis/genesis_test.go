138006023ff67ca9d90ae2e48cfb87fe95c7247b
Christopher Hall <hsw@bitmark.com>
2016-06-13 10:19:52 +0800

    [genesis] make sample blocks for testing
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- genesis/genesis_test.go
+++ genesis/genesis_test.go
@@ -6,135 +6,161 @@ package genesis_test
 
 import (
 	"bytes"
-	"encoding/binary"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
+	//"encoding/binary"
+	//"encoding/hex"
+	//"encoding/json"
+	//"fmt"
+	"github.com/bitmark-inc/bitmarkd/account"
+	block "github.com/bitmark-inc/bitmarkd/block2"
 	"github.com/bitmark-inc/bitmarkd/blockdigest"
+	"github.com/bitmark-inc/bitmarkd/blockrecord"
+	"github.com/bitmark-inc/bitmarkd/chain"
+	"github.com/bitmark-inc/bitmarkd/currency"
 	"github.com/bitmark-inc/bitmarkd/difficulty"
 	"github.com/bitmark-inc/bitmarkd/genesis"
 	"github.com/bitmark-inc/bitmarkd/merkle"
+	"github.com/bitmark-inc/bitmarkd/mode"
+	"github.com/bitmark-inc/bitmarkd/transactionrecord"
 	"github.com/bitmark-inc/bitmarkd/util"
-	"strings"
+	//"strings"
 	"testing"
 	"time"
 )
 
 // some constants embedded into the genesis block
 const (
-	genesisBlockNumber = uint64(1)
+	genesisBlockNumber  = 1
+	genesisBlockVersion = 1
 )
+
+// hold chain specific timestamp
+type TS struct {
+	timestamp uint64
+	utc       string
+}
+
+// data block
+type SourceData struct {
+	Timestamp TS
+	Nonce     blockrecord.NonceType
+	Nonce2    uint64
+	Message   string
+	ProofedBy string
+	Signature []byte
+}
 
 // some data embedded into the genesis block
 // for live chain
-var (
+var LiveNet = SourceData{
 	// date -u -r $(printf '%d\n' 0x56809ab7)
 	// Mon 28 Dec 2015 02:13:11 UTC
 	// date -u -r $(printf '%d\n' 0x56809ab7) '+%FT%TZ'
 	// 2015-12-28T02:13:11Z
+	Timestamp: TS{0x56809ab7, "2015-12-28T02:13:11Z"},
 
-	genesisLiveTimestamp = TS{0x56809ab7, "2015-12-28T02:13:11Z"}
+	Nonce: 0x4fa9713e80a6d2ed,
 
-	genesisLiveAddresses = []block.MinerAddress{
-		{
-			Currency: "",
-			Address:  "DOWN the RABBIT hole",
-		},
-	}
-	genesisLiveRawAddress = "\x00\x14" + "DOWN the RABBIT hole"
-)
+	Nonce2: 0x4c6976652a4e6574,
+
+	Message: "DOWN the RABBIT hole",
+
+	ProofedBy: "acRQJLJtHH61bfoQydREnvXDQ4Tt2BLmGbP1UbcFpJouJSM5hG",
+
+	Signature: []byte{
+		0xc2, 0xa3, 0x84, 0xeb, 0xc9, 0x01, 0xa1, 0x8a,
+		0x13, 0xa2, 0x70, 0xaa, 0x9f, 0x5e, 0x08, 0x06,
+		0x77, 0xd7, 0xab, 0x2f, 0xd8, 0x88, 0xa5, 0xf6,
+		0x57, 0xd2, 0xc6, 0xd4, 0x69, 0x2e, 0x6f, 0xcd,
+		0xe7, 0x1c, 0x04, 0xb9, 0x1b, 0xe1, 0x40, 0x0e,
+		0x7c, 0x1e, 0x8d, 0x5e, 0x2b, 0x34, 0x83, 0xc4,
+		0x77, 0xfe, 0xa1, 0x7b, 0xc1, 0xde, 0xe0, 0x05,
+		0xcc, 0x8d, 0x4d, 0xf8, 0x62, 0x77, 0x0d, 0x0c,
+	},
+}
 
 // some data embedded into the genesis block
 // for test chain
-var (
+var TestNet = SourceData{
 	// date -u -r $(printf '%d\n' 0x5478424b)
 	// Fri Nov 28 09:37:15 UTC 2014
 	// date -u -r $(printf '%d\n' 0x5478424b) '+%FT%TZ'
 	// 2014-11-28T09:37:15Z
+	Timestamp: TS{0x5478424b, "2014-11-28T09:37:15Z"},
 
-	genesisTestTimestamp = TS{0x5478424b, "2014-11-28T09:37:15Z"}
+	Nonce: 0x473640eeca2b4cd4,
 
-	// for testing chain
-	genesisTestAddresses = []block.MinerAddress{
-		{
-			Currency: "",
-			Address:  "Bitmark Testing Genesis Block",
-		},
-	}
-	genesisTestRawAddress = "\x00\x1d" + "Bitmark Testing Genesis Block"
-)
+	Nonce2: 0x546573742a4e6574,
+
+	Message: "Bitmark Testing Genesis Block",
+
+	ProofedBy: "fHrBioy1AMn86jJj1rk5j5rokqQhz8hABmccHjfxp9JkAF1dJz",
+
+	Signature: []byte{
+		0x02, 0xa8, 0xbf, 0x5c, 0x21, 0x73, 0x03, 0x24,
+		0x04, 0x40, 0x79, 0xa5, 0x78, 0x0a, 0x9c, 0xd2,
+		0x2f, 0xc2, 0x22, 0xb4, 0x4c, 0x91, 0x29, 0x17,
+		0xce, 0xa5, 0xb9, 0xd3, 0x77, 0x0c, 0x13, 0x8e,
+		0x8d, 0x3e, 0xae, 0x98, 0xb7, 0x6c, 0x2e, 0x93,
+		0xa9, 0x7e, 0x41, 0xc4, 0x1b, 0xae, 0x36, 0xc8,
+		0x41, 0x37, 0x08, 0xa9, 0x94, 0xfe, 0xc2, 0xf9,
+		0xeb, 0xc0, 0xf8, 0x02, 0x98, 0x3d, 0xf6, 0x01,
+	},
+}
 
 // create the live genesis block
 //
-// MinerUsername: "miner-arm"  JobId: "Live_efd7b4fe"  ExtraNonce2: "00000000"  Ntime: "56809ab7"  Nonce: "826f9a87"
+// must be first
 func TestLiveGenesisAssembly(t *testing.T) {
-
-	// fixed data used to create genesis block
-	// ---------------------------------------
-
-	// nonce provided by statum sserver
-	extraNonce1 := []byte{0xef, 0xd7, 0xb4, 0xfe}
-
-	// nonces obtained from miner
-	nonce := uint32(0x826f9a87)
-	extraNonce2 := []byte{0x00, 0x00, 0x00, 0x00}
-
-	doCalc(t, "Live", genesisLiveTimestamp, extraNonce1, extraNonce2, nonce, genesisLiveAddresses, genesisLiveRawAddress, genesis.LiveGenesisDigest, genesis.LiveGenesisBlock)
+	checkAssembly(t, "Live", LiveNet, genesis.LiveGenesisDigest, genesis.LiveGenesisBlock)
 }
 
 // create the test genesis block
 //
-// MinerUsername: "miner-arm"  JobId: "Test_b201475b"  ExtraNonce2: "00000000"  Ntime: "5478424b"  Nonce: "1e26bad4"
+// must be after the live test
 func TestTestGenesisAssembly(t *testing.T) {
 
-	// fixed data used to create genesis block
-	// ---------------------------------------
+	mode.Initialise(chain.Testing) // enter test mode - ONLY ALLOWED ONCE (or panic will occur
 
-	// nonce provided by statum server
-	extraNonce1 := []byte{0xb2, 0x01, 0x47, 0x5b}
-
-	// nonces obtained from miner
-	nonce := uint32(0x1e26bad4)
-	extraNonce2 := []byte{0x00, 0x00, 0x00, 0x00}
-
-	doCalc(t, "Test", genesisTestTimestamp, extraNonce1, extraNonce2, nonce, genesisTestAddresses, genesisTestRawAddress, genesis.TestGenesisDigest, genesis.TestGenesisBlock)
+	checkAssembly(t, "Test", TestNet, genesis.TestGenesisDigest, genesis.TestGenesisBlock)
 }
 
-// hold chain specific timestamp
-type TS struct {
-	ntime uint32
-	utc   string
-}
+func checkAssembly(t *testing.T, title string, source SourceData, gDigest blockdigest.Digest, gBlock []byte) {
 
-func doCalc(t *testing.T, title string, ts TS, extraNonce1 []byte, extraNonce2 []byte, nonce uint32, addresses []block.MinerAddress, rawAddress string, gDigest blockdigest.Digest, gBlock blockrecord.PackedHeader) {
-
-	timestamp, err := time.Parse(time.RFC3339, ts.utc)
+	proofedbyAccount, err := account.AccountFromBase58(source.ProofedBy)
 	if nil != err {
-		t.Fatalf("failed to parse time: err = %v", err)
+		t.Fatalf("failed to parse account: error: %v", err)
+	}
+
+	timestamp, err := time.Parse(time.RFC3339, source.Timestamp.utc)
+	if nil != err {
+		t.Fatalf("failed to parse time: error: %v", err)
 	}
 	timeUint64 := uint64(timestamp.UTC().Unix())
-	ntime := ts.ntime
-	if timeUint64 != uint64(ntime) {
-		t.Fatalf("time converted to: 0x%08x  expectd: %08x", timeUint64, ntime)
+	if timeUint64 != source.Timestamp.timestamp {
+		t.Fatalf("time converted to: 0x%x  expected: %x", timeUint64, source.Timestamp.timestamp)
 	}
 
 	// some common static data
-	version := uint32(1) // snapshot of version number
 	previousBlock := blockdigest.Digest{}
 
-	// Just calculations after this point
-	// ----------------------------------
+	b := transactionrecord.BaseData{
+		Currency:       currency.Nothing,
+		PaymentAddress: source.Message,
+		Owner:          proofedbyAccount,
+		Nonce:          source.Nonce2,
+		Signature:      source.Signature,
+	}
 
-	coinbase := block.NewFullCoinbase(genesisBlockNumber, timestamp, append(extraNonce1, extraNonce2...), addresses)
-	cDigest := merkle.NewDigest(coinbase)
-	coinbaseLength := len(coinbase)
-
-	transactionCount := 1
+	base, err := b.Pack(proofedbyAccount)
+	if nil != err {
+		t.Fatalf("failed to pack base: error: %v", err)
+	}
+	baseDigest := merkle.Digest(base.MakeLink())
 
 	// merkle tree
-	tree := merkle.FullMerkleTree(cDigest, []merkle.Digest{})
-	if tree[len(tree)-1] != cDigest {
-		t.Fatalf("failed to compute tree: actual: %#v  expected: %#v", tree[len(tree)-1], cDigest)
+	tree := merkle.FullMerkleTree([]merkle.Digest{baseDigest})
+	if tree[len(tree)-1] != baseDigest {
+		t.Fatalf("failed to compute tree: actual: %#v  expected: %#v", tree[len(tree)-1], baseDigest)
 	}
 
 	// default difficulty
@@ -142,12 +168,14 @@ func doCalc(t *testing.T, title string, ts TS, extraNonce1 []byte, extraNonce2 [
 
 	// block header
 	h := blockrecord.Header{
-		Version:       version,
-		PreviousBlock: previousBlock,
-		MerkleRoot:    tree[len(tree)-1],
-		Time:          ntime,
-		Bits:          *bits,
-		Nonce:         nonce,
+		Version:          genesisBlockVersion,
+		TransactionCount: 1,
+		Number:           genesisBlockNumber,
+		PreviousBlock:    previousBlock,
+		MerkleRoot:       tree[len(tree)-1], // replace with message?
+		Timestamp:        source.Timestamp.timestamp,
+		Difficulty:       bits,
+		Nonce:            source.Nonce,
 	}
 
 	header := h.Pack()
@@ -157,104 +185,19 @@ func doCalc(t *testing.T, title string, ts TS, extraNonce1 []byte, extraNonce2 [
 	t.Logf("Title: %s", title)
 	t.Logf("header: %#v\n", h)
 	t.Logf("packed header: %x\n", header)
-	t.Logf("coinbase: %x\n", coinbase)
-	t.Logf("coinbase digest: %#v\n", cDigest)
+	t.Logf("base: %x\n", base)
+	t.Logf("base digest: %#v\n", baseDigest)
 	t.Logf("merkle tree: %#v\n", tree)
 	t.Logf("merkle root little endian hex: %x\n", [blockdigest.Length]byte(tree[0]))
 	t.Logf("hDigest: %#v\n", hDigest)
 	t.Logf("hDigest little endian hex: %x\n", [blockdigest.Length]byte(hDigest))
 
-	t.Log(util.FormatBytes(title+"ProposedLEhash", []byte(hDigest[:])))
-
-	// chack that it matches
+	// check that it matches
 	if hDigest != gDigest {
 		t.Errorf("digest mismatch actual: %#v  expected: %#v", hDigest, gDigest)
+		//t.Log(util.FormatBytes(title+"ProposedBlockHeader", header))
+		t.Log(util.FormatBytes(title+"ProposedLEhash", []byte(hDigest[:])))
 
-		hexExtraNonce1 := fmt.Sprintf("%08x", extraNonce1)
-		hexCoinbase := hex.EncodeToString(coinbase)
-		n1 := strings.Index(hexCoinbase, hexExtraNonce1)
-		n2 := n1 + 2*(len(extraNonce1)+len(extraNonce2)) // since 2 hex chars = 1 byte
-		login := []struct {
-			ID     interface{}   `json:"id"`
-			Method string        `json:"method"`
-			Result interface{}   `json:"result"`
-			Error  []interface{} `json:"error"`
-		}{
-			{
-				ID:     1,
-				Method: "mining.subscribe",
-				Result: []interface{}{
-					[][]string{
-						{"mining.set_difficulty", "1357"},
-						{"mining.notify", "1234"},
-					},
-					hexExtraNonce1,
-					len(extraNonce2),
-				},
-			},
-			{
-				ID:     2,
-				Method: "mining.authorize",
-				Result: true,
-			},
-			{
-				ID:     3,
-				Method: "mining.extranonce.subscribe",
-				Result: true,
-			},
-		}
-
-		requests := []struct {
-			Method string        `json:"method"`
-			Params []interface{} `json:"params"`
-		}{
-			{
-				Method: "mining.set_difficulty",
-				Params: []interface{}{
-					difficulty.Current.Reciprocal(),
-				},
-			},
-			{
-				Method: "mining.set_extranonce",
-				Params: []interface{}{
-					"08000002",
-					4,
-				},
-			},
-			{
-				Method: "mining.notify",
-				Params: []interface{}{
-					title, // [0] job_id
-					fmt.Sprintf("%s", previousBlock), // [1] previous link
-					hexCoinbase[:n1],                 // [2] coinbase 1
-					hexCoinbase[n2:],                 // [3] coinbase 2
-					[]interface{}{},                  // [4] minimised merkle tree (empty)
-					fmt.Sprintf("%08x", version),     // [5] version
-					difficulty.Current.String(),      // [6] bits
-					fmt.Sprintf("%08x", ntime),       // [7] time
-					true, // [8] clean_jobs
-				},
-			},
-		}
-
-		for _, r := range login {
-			b, err := json.Marshal(r)
-			if nil != err {
-				t.Errorf("json error: %v", err)
-				return
-			}
-
-			t.Logf("JSON: %s", b)
-		}
-		for _, r := range requests {
-			b, err := json.Marshal(r)
-			if nil != err {
-				t.Errorf("json error: %v", err)
-				return
-			}
-
-			t.Logf("JSON: %s", b)
-		}
 	}
 
 	// check difficulty
@@ -263,120 +206,105 @@ func doCalc(t *testing.T, title string, ts TS, extraNonce1 []byte, extraNonce2 [
 	}
 
 	// compute block size
-	blockSize := len(header) + 2 + coinbaseLength + 2 + len(tree)*merkle.DigestLength
+	blockSize := len(header) + len(base)
 
 	// pack the block
-	blk := make([]byte, 0, blockSize)
+	blk := block.Packed(make([]byte, 0, blockSize))
 	blk = append(blk, header...)
-	blk = append(blk, byte(coinbaseLength&0xff))
-	blk = append(blk, byte(coinbaseLength>>8))
-	blk = append(blk, coinbase...)
-	blk = append(blk, byte(transactionCount&0xff))
-	blk = append(blk, byte(transactionCount>>8))
-
-	buffer := new(bytes.Buffer)
-	err = binary.Write(buffer, binary.LittleEndian, tree)
-	if nil != err {
-		t.Fatalf("binary.Write: err = %v", err)
-	}
-
-	blk = append(blk, buffer.Bytes()...)
-
-	if len(blk) != blockSize {
-		t.Fatalf("block size mismatch: actual: %d, expected: %d", len(blk), blockSize)
-	}
+	blk = append(blk, base...)
 
 	if !bytes.Equal(blk, gBlock) {
 		t.Errorf("initial block assembly mismatch actual: %x  expected: %x", blk, gBlock)
 		t.Log(util.FormatBytes(title+"GenesisBlock", blk))
 	}
 
+	t.Logf("packed block: %x", blk)
+
 	// unpack the block
-	var unpacked block.Block
-	err = blockrecord.PackedHeader(blk).Unpack(&unpacked)
+	unpacked, err := blk.Unpack()
 	if nil != err {
-		t.Fatalf("unpack block failed: err = %v", err)
+		t.Fatalf("unpack block failed: error: %v", err)
 	}
 
-	if unpacked.Header.Time != ntime {
-		t.Fatalf("block ntime mismatch: actual 0x%08x  expected 0x%08x", unpacked.Header.Time, ntime)
+	if unpacked.Header.Timestamp != h.Timestamp {
+		t.Fatalf("block ntime mismatch: actual 0x%08x  expected 0x%08x", unpacked.Header.Timestamp, h.Timestamp)
 	}
 
-	if unpacked.Timestamp != timestamp {
-		t.Fatalf("block timestamp mismatch: actual %v  expected %v", unpacked.Timestamp, timestamp)
-	}
+	// 	if unpacked.Timestamp != timestamp {
+	// 		t.Fatalf("block timestamp mismatch: actual %v  expected %v", unpacked.Timestamp, timestamp)
+	// 	}
 
 	t.Logf("unpacked block: %#v", unpacked)
 
-	// re-pack
-	reDigest, rePacked, ok := block.Pack(unpacked.Number, timestamp, &unpacked.Header.Bits, unpacked.Header.Time, unpacked.Header.Nonce, append(extraNonce1, extraNonce2...), unpacked.Addresses, unpacked.TxIds)
+	// ***** FIX THIS: is block.Pack() really necessary, need to determinal all the previous owners
+	// ***** FIX THIS: suggest not; just keep tx in packed form
+	//      // re-pack
+	// 	reDigest, rePacked, ok := block.Pack(unpacked.Number, timestamp, &unpacked.Header.Bits, unpacked.Header.Time, unpacked.Header.Nonce, append(extraNonce1, extraNonce2...), unpacked.Addresses, unpacked.TxIds)
 
-	if !ok {
-		t.Fatal("block.Pack failed")
-	}
+	// 	if !ok {
+	// 		t.Fatal("block.Pack failed")
+	// 	}
 
-	if reDigest != gDigest {
-		t.Fatalf("re-digest mismatch actual: %#v  expected: %#v", reDigest, gDigest)
-	}
+	// 	if reDigest != gDigest {
+	// 		t.Fatalf("re-digest mismatch actual: %#v  expected: %#v", reDigest, gDigest)
+	// 	}
 
-	if !bytes.Equal(rePacked, blk) {
-		t.Fatalf("re-packed mismatch actual: %x  expected: %x", rePacked, blk)
-	}
+	// 	if !bytes.Equal(rePacked, blk) {
+	// 		t.Fatalf("re-packed mismatch actual: %x  expected: %x", rePacked, blk)
+	// 	}
 
-	// log the final result
-	if verboseTesting { // turn on in all_test.go
-		t.Logf("Genesis digest: %#v", reDigest)
-		t.Logf("Genesis block:  %x", rePacked)
-	}
+	// 	// log the final result
+	// 	t.Logf("Genesis digest: %#v", reDigest)
+	// 	t.Logf("Genesis block:  %x", rePacked)
 
-	// hex dumps for genesis.go
-	t.Log(formatBytes(title+"GenesisBlock", rePacked))
-	t.Log(formatBytes(title+"GenesisDigest", reDigest[:]))
+	// 	// hex dumps for genesis.go
+	// 	t.Log(util.FormatBytes(title+"GenesisBlock", rePacked))
+	// 	t.Log(util.FormatBytes(title+"GenesisDigest", reDigest[:]))
 
-	// check that these match the current genesis block/digest
-	if reDigest != gDigest {
-		t.Fatalf("re-digest/Genesis mismatch actual: %#v  expected: %#v", reDigest, gDigest)
-	}
+	// 	// check that these match the current genesis block/digest
+	// 	if reDigest != gDigest {
+	// 		t.Fatalf("re-digest/Genesis mismatch actual: %#v  expected: %#v", reDigest, gDigest)
+	// 	}
 
-	if !bytes.Equal(rePacked, gBlock) {
-		t.Fatalf("re-packed/Genesis mismatch actual: %x  expected: %x", rePacked, gBlock)
-	}
-}
+	// 	if !bytes.Equal(rePacked, gBlock) {
+	// 		t.Fatalf("re-packed/Genesis mismatch actual: %x  expected: %x", rePacked, gBlock)
+	// 	}
+	// }
 
-// test the real genesis block
-func TestGenesisBlock(t *testing.T) {
-	doReal(t, "Live", block.LiveGenesisDigest, block.LiveGenesisBlock)
-	doReal(t, "Test", block.TestGenesisDigest, block.TestGenesisBlock)
-}
+	// // test the real genesis block
+	// func TestGenesisBlock(t *testing.T) {
+	// 	doReal(t, "Live", block.LiveGenesisDigest, block.LiveGenesisBlock)
+	// 	doReal(t, "Test", block.TestGenesisDigest, block.TestGenesisBlock)
+	// }
 
-func doReal(t *testing.T, title string, gDigest blockdigest.Digest, gBlock blockrecord.PackedHeader) {
+	// func doReal(t *testing.T, title string, gDigest blockdigest.Digest, gBlock blockrecord.PackedHeader) {
 
-	// unpack the block
-	var unpacked block.Block
-	err := block.LiveGenesisBlock.Unpack(&unpacked)
-	if nil != err {
-		t.Fatalf("unpack block failed: err = %v", err)
-	}
+	// 	// unpack the block
+	// 	var unpacked block.Block
+	// 	err := block.LiveGenesisBlock.Unpack(&unpacked)
+	// 	if nil != err {
+	// 		t.Fatalf("unpack block failed: error: %v", err)
+	// 	}
 
-	if verboseTesting { // turn on in all_test.go
-		t.Logf("unpacked block: %v", unpacked)
-	}
+	// 	if verboseTesting { // turn on in all_test.go
+	// 		t.Logf("unpacked block: %v", unpacked)
+	// 	}
 
-	// check current genesis digest matches
-	if unpacked.Digest != block.LiveGenesisDigest {
-		t.Fatalf("digest/Genesis mismatch actual: %#v  expected: %#v", unpacked.Digest, block.LiveGenesisDigest)
-	}
+	// 	// check current genesis digest matches
+	// 	if unpacked.Digest != block.LiveGenesisDigest {
+	// 		t.Fatalf("digest/Genesis mismatch actual: %#v  expected: %#v", unpacked.Digest, block.LiveGenesisDigest)
+	// 	}
 
-	// check block number
-	if unpacked.Number != genesisBlockNumber {
-		t.Fatalf("block number: %d  expected %d", unpacked.Number, genesisBlockNumber)
-	}
+	// 	// check block number
+	// 	if unpacked.Number != genesisBlockNumber {
+	// 		t.Fatalf("block number: %d  expected %d", unpacked.Number, genesisBlockNumber)
+	// 	}
 
-	// check the address matches
-	if 1 != len(unpacked.Addresses) {
-		t.Fatalf("Addresses: found: %d  expected: %d", len(unpacked.Addresses), 1)
-	}
-	if unpacked.Addresses[0].String() != genesisLiveRawAddress {
-		t.Fatalf("RawAddress: %q  expected: %q", unpacked.Addresses[0].String(), genesisLiveRawAddress)
-	}
+	// 	// check the address matches
+	// 	if 1 != len(unpacked.Addresses) {
+	// 		t.Fatalf("Addresses: found: %d  expected: %d", len(unpacked.Addresses), 1)
+	// 	}
+	// 	if unpacked.Addresses[0].String() != genesisLiveRawAddress {
+	// 		t.Fatalf("RawAddress: %q  expected: %q", unpacked.Addresses[0].String(), genesisLiveRawAddress)
+	// 	}
 }
