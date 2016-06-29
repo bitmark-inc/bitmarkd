431f2667e277876eb54c316f68d28a80e9706ca8
Christopher Hall <hsw@bitmark.com>
2016-06-29 13:43:41 +0800

    [pool] remove old pool module
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- pool/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2014-2016 Bitmark Inc.
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
-// maintain severa pools of a number of elements in key->value form
-//
-// This maintains a LevelDB database split into a series of tables.
-// Each table is defined by a prefix byte.
-package pool
