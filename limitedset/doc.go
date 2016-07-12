4972d3a6dbcb1b885bb2cb236457fa5dfe1bd3b3
Christopher Hall <hsw@bitmark.com>
2016-07-12 15:23:28 +0800

    [gnomon,limitedset,peer,transaction] remove old code
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- limitedset/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2014-2016 Bitmark Inc.
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
-// this is a limited size set
-//
-// items may only be added or check for existance
-// items are automatically removed on a first-in first-out basis
-package limitedset
--- peer/doc.go
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2014-2016 Bitmark Inc.
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
-// P2P client/server system for bitmarkd
-//
-// This used RPC to send message to connected peers using the
-// bilateralrpc module.
-package peer
