aa84cdbbb57adf5c03a15a35763d2636ecf01c05
Jim Yeh <jim@bitmark.com>
2017-03-13 15:18:32 +0800

    [avl] add methods for avl tree for additional supports

--- avl/search.go
+++ avl/search.go
@@ -23,3 +23,29 @@ func search(key item, tree *Node) *Node {
 		return tree
 	}
 }
+
+// get the order of a node with a specific key in a tree
+func (p *Node) GetOrder(key item) uint {
+	iterNode := p.first()
+	lastKey := p.last().Key()
+	order := uint(0)
+
+	for iterNode.Key().Compare(key) != 0 && iterNode.Key().Compare(lastKey) != 0 {
+		order += 1
+		iterNode = iterNode.Next()
+	}
+
+	return order
+}
+
+// get the node of a tree in order
+func (p *Node) GetNodeByOrder(order uint) *Node {
+	node := p.first()
+	for i := uint(0); i < order; i++ {
+		if node == p.last() {
+			break
+		}
+		node = node.Next()
+	}
+	return node
+}
--- avl/setup.go
+++ avl/setup.go
@@ -28,6 +28,31 @@ func (tree *Tree) Count() int {
 	return tree.count
 }
 
+// Root return the root node of the tree
+func (tree *Tree) Root() *Node {
+	return tree.root
+}
+
+// GetChildrenByDepth will returns all children in a specific depth of a tree
+func (p *Node) GetChildrenByDepth(depth uint) []*Node {
+	nodes := []*Node{}
+
+	if depth == 0 {
+		nodes = []*Node{p}
+	} else {
+		left := p.left
+		right := p.right
+		if left != nil {
+			nodes = append(nodes, left.GetChildrenByDepth(depth-1)...)
+		}
+
+		if right != nil {
+			nodes = append(nodes, right.GetChildrenByDepth(depth-1)...)
+		}
+	}
+	return nodes
+}
+
 // read the key from a node
 func (p *Node) Key() item {
 	return p.key
@@ -36,4 +61,20 @@ func (p *Node) Key() item {
 // read the value from a node
 func (p *Node) Value() interface{} {
 	return p.value
+}
+
+// return parent node of a node
+func (p *Node) Parent() *Node {
+	return p.up
+}
+
+// get the depth of a node
+func (p *Node) Depth() uint {
+	count := uint(0)
+	parent := p.up
+	for parent != nil {
+		count += 1
+		parent = parent.up
+	}
+	return count
 }
