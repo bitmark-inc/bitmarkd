d247886ee75520bb9ae586e3b6b75e68b7cce8a7
Christopher Hall <hsw@bitmark.com>
2017-04-20 17:46:45 +0800

    new version
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- version.go
+++ version.go
@@ -6,7 +6,7 @@ package main
 
 // ensure that git has a tag: "vX.Y" corresponding to major and minor
 const (
-	Major   = "3"
-	Minor   = "6"
+	Major   = "4"
+	Minor   = "0"
 	Version = Major + "." + Minor
 )
