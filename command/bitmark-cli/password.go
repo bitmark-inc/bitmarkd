0b48a124de191fa062245634537600430facd385
Christopher Hall <hsw@bitmark.com>
2018-01-15 14:43:59 +0800

    update year in headers
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- command/bitmark-cli/password.go
+++ command/bitmark-cli/password.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2017 Bitmark Inc.
+// Copyright (c) 2014-2018 Bitmark Inc.
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
--- util/base58.go
+++ util/base58.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2013-2014 Conformal Systems LLC.
-// Copyright (c) 2014-2017 Bitmark Inc.
+// Copyright (c) 2014-2018 Bitmark Inc.
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
