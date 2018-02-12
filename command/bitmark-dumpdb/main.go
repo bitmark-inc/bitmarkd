b7ff08919b6bb7d8b1204ed8ec749b1e03d03790
Christopher Hall <hsw@bitmark.com>
2018-02-12 16:03:09 +0800

    [command] colour the output, add delete option
    This allows selected records to be deleted from the data tables
    for testing purposes.
    
    Signed-off-by: Christopher Hall <hsw@bitmark.com>

--- command/bitmark-dumpdb/main.go
+++ command/bitmark-dumpdb/main.go
@@ -5,16 +5,72 @@
 package main
 
 import (
+	"bytes"
+	"encoding/hex"
 	"fmt"
 	"github.com/bitmark-inc/bitmarkd/storage"
+	"github.com/bitmark-inc/exitwithstatus"
+	"github.com/bitmark-inc/getoptions"
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
 )
 
+// set by the linker: go build -ldflags "-X main.version=M.N" ./...
+var version string = "zero" // do not change this value
+
+// tags for the signing key data
+const (
+	taggedSeed    = "SEED:"    // followed by base58 encoded seed as produced by desktop/cli client
+	taggedPrivate = "PRIVATE:" // followed by 64 bytes of hex Ed25519 private key
+)
+
+// colours
+const (
+	keyColour1  = "\033[1;36m"
+	keyColour2  = "\033[1;31m"
+	valColour1  = "\033[1;33m"
+	valColour2  = "\033[1;34m"
+	delColour1  = "\033[1;35m"
+	delColour2  = "\033[0;35m"
+	delColour3  = "\033[0;31m"
+	delColour4  = "\033[1;35m"
+	nodelColour = "\033[1;32m"
+	endColour   = "\033[0m"
+)
+
+// main program
 func main() {
-	if len(os.Args) < 4 {
-		fmt.Printf("usage: dumpdb database tag count\n")
+	// ensure exit handler is first
+	defer exitwithstatus.Handler()
+
+	flags := []getoptions.Option{
+		{Long: "help", HasArg: getoptions.NO_ARGUMENT, Short: 'h'},
+		{Long: "verbose", HasArg: getoptions.NO_ARGUMENT, Short: 'v'},
+		{Long: "version", HasArg: getoptions.NO_ARGUMENT, Short: 'V'},
+		{Long: "list", HasArg: getoptions.NO_ARGUMENT, Short: 'l'},
+		{Long: "delete", HasArg: getoptions.NO_ARGUMENT, Short: 'd'},
+		{Long: "early", HasArg: getoptions.NO_ARGUMENT, Short: 'e'},
+		{Long: "colour", HasArg: getoptions.NO_ARGUMENT, Short: 'g'},
+		{Long: "file", HasArg: getoptions.REQUIRED_ARGUMENT, Short: 'f'},
+		{Long: "count", HasArg: getoptions.REQUIRED_ARGUMENT, Short: 'c'},
+	}
+
+	program, options, arguments, err := getoptions.GetOS(flags)
+	if nil != err {
+		exitwithstatus.Message("%s: getoptions error: %s", program, err)
+	}
+
+	if len(options["version"]) > 0 {
+		exitwithstatus.Message("%s: version: %s", program, version)
+	}
+
+	if len(options["help"]) > 0 || 0 == len(arguments) || 1 != len(options["file"]) {
+		exitwithstatus.Message("usage: %s [--help] [--verbose] [--quiet] [--count=N] --file=FILE tag [key-prefix]", program)
+	}
+
+	if len(options["list"]) > 0 {
 
 		// this will be a struct type
 		poolType := reflect.TypeOf(storage.Pool)
@@ -26,19 +82,41 @@ func main() {
 			prefixTag := fieldInfo.Tag.Get("prefix")
 			fmt.Printf("       %s → %s\n", prefixTag, fieldInfo.Name)
 		}
-		return
 	}
 
-	filename := os.Args[1]
+	// stop if prefix no longer matches
+	earlyStop := len(options["early"]) > 0
 
-	tag := os.Args[2]
+	colour := len(options["colour"]) > 0
+	delete := len(options["delete"]) > 0
+	verbose := len(options["verbose"]) > 0
 
-	count, err := strconv.Atoi(os.Args[3])
-	if nil != err {
-		fmt.Printf("Error on Fetch: %v\n", err)
-		return
+	count := 10
+	if len(options["count"]) > 0 {
+		count, err = strconv.Atoi(options["count"][0])
+		if nil != err {
+			exitwithstatus.Message("%s: convert count error: %s", program, err)
+		}
+		if count < 1 {
+			exitwithstatus.Message("%s: invalid count: %d", program, count)
+		}
 	}
 
+	filename := options["file"][0]
+	tag := arguments[0]
+	if verbose {
+		fmt.Printf("read tag: %s from file: %q\n", tag, filename)
+	}
+
+	prefix := []byte(nil)
+	if len(arguments) > 1 {
+		prefix, err = hex.DecodeString(arguments[1])
+		if nil != err {
+			exitwithstatus.Message("%s: convert prefix error: %s\n", program, err)
+		}
+	}
+
+	// start of main processing
 	storage.Initialise(filename)
 	defer storage.Finalise()
 
@@ -63,19 +141,84 @@ func main() {
 
 	}
 	if nil == p {
-		fmt.Printf("no pool corresponding to: %q\n", tag)
-		return
+		exitwithstatus.Message("%s: no pool corresponding to: %q\n", program, tag)
 	}
 
 	// dump the items as hex
 	cursor := p.NewFetchCursor()
+	if len(prefix) > 0 {
+		cursor.Seek(prefix)
+	}
+
 	data, err := cursor.Fetch(count)
 	if nil != err {
-		fmt.Printf("Error on Fetch: %v\n", err)
-		return
+		exitwithstatus.Message("%s: error on Fetch: %s\n", program, err)
 	}
+
+	l := len(prefix)
+
+	ck1 := ""
+	ck2 := ""
+	cv1 := ""
+	cv2 := ""
+	cd1 := ""
+	cd2 := ""
+	cd3 := ""
+	cd4 := ""
+	cn := ""
+	ce := ""
+	if colour {
+		ck1 = keyColour1
+		ck2 = keyColour2
+		cv1 = valColour1
+		cv2 = valColour2
+		cd1 = delColour1
+		cd2 = delColour2
+		cd3 = delColour3
+		cd4 = delColour4
+		cn = nodelColour
+		ce = endColour
+	}
+print_loop:
 	for i, e := range data {
-		fmt.Printf("%d: Key: %x\n", i, e.Key)
-		fmt.Printf("%d: Val: %x\n", i, e.Value)
+		if earlyStop && len(e.Key) >= len(prefix) && !bytes.Equal(prefix, e.Key[:l]) {
+			fmt.Printf("*** early stop\n")
+			break print_loop
+		}
+
+		fmt.Printf("%d: %sKey: %s%x%s\n", i, ck1, ck2, e.Key, ce)
+		fmt.Printf("%d: %sVal: %s%x%s\n", i, cv1, cv2, e.Value, ce)
+		if delete {
+		delete_loop:
+			for {
+				fmt.Printf("%d: %sDelete Key: %s%x%s ? [yNq]: ", i, cd1, cd2, e.Key, ce)
+
+				buffer := make([]byte, 100)
+				n, err := os.Stdin.Read(buffer)
+				if nil != err {
+					exitwithstatus.Message("%s: error on Stdin.Read: %e\n", program, err)
+				}
+
+				response := strings.TrimSpace(string(buffer[:n]))
+				switch strings.ToLower(response) {
+
+				case "y", "yes":
+					p.Delete(e.Key)
+					fmt.Printf("%d: %s***DELETED: %s%x%s\n", i, cd3, cd4, e.Key, ce)
+					break delete_loop
+
+				case "", "n", "no":
+					fmt.Printf("%d: %sRetain Key: %s%x%s\n", i, cn, ck2, e.Key, ce)
+					break delete_loop
+
+				case "q", "quit", "e", "exit", "x":
+					fmt.Printf("Terminated\n")
+					return
+
+				default:
+					fmt.Printf("Please answer yes or no\n")
+				}
+			}
+		}
 	}
 }
