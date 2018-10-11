7ecd0448cb7fba0dc83d6bd4ba9b436c6f7bac72
Aaron Chang <aaron.chang@quadas.com>
2018-10-11 12:05:42 +0800

    [test] add missing test/document for packages

---
+++ command/bitmark-dumpdb/doc.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2014-2018 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+// Bitmark bitmark-dumpdb
+package main
---
+++ mode/mode_test.go
@@ -0,0 +1,5 @@
+// Copyright (c) 2014-2018 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package mode
