8a0bdef4f750707e0396b347bcd91bac3921bd95
Aaron Chang <xesuschang@gmail.com>
2019-03-19 17:38:45 +0800

    [recorderd] add job manager

---
+++ command/recorderd/job_manager.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"sync"
+	"time"
+
+	"github.com/bitmark-inc/logger"
+)
+
+type JobManager interface {
+	Start()
+}
+
+const (
+	JobManagerPrefix = "job-manager"
+)
+
+type JobManagerChannel struct {
+	rescheduleChannel chan struct{}
+	startEventChannel chan struct{}
+	stopEventChannel  chan struct{}
+}
+
+type JobManagerData struct {
+	calendar    JobCalendar
+	proofer     Proofer
+	channels    JobManagerChannel
+	log         *logger.L
+	initialized bool
+	wg          sync.WaitGroup
+}
+
+func newJobManager(calendar JobCalendar, proofer Proofer, rescheduleChannel chan struct{}, logger *logger.L) JobManager {
+	return &JobManagerData{
+		calendar: calendar,
+		proofer:  proofer,
+		log:      logger,
+		channels: JobManagerChannel{
+			rescheduleChannel: rescheduleChannel,
+			startEventChannel: make(chan struct{}, 1),
+			stopEventChannel:  make(chan struct{}, 1),
+		},
+		initialized: false,
+	}
+}
+
+func (j *JobManagerData) waitForRefresh() {
+	j.log.Info("waiting to reschedule events")
+	for {
+		select {
+		case <-j.channels.rescheduleChannel:
+			j.log.Debug("receive reschedule event")
+			if j.initialized {
+				j.resetAllEvent()
+			} else {
+				j.initialized = true
+			}
+			j.reschedule()
+		}
+	}
+	j.log.Info("stop...")
+}
+
+func (j *JobManagerData) waitNextHasingStartEvent(duration time.Duration) {
+	j.log.Infof("create goroutine for start events, next event duration: %.1f minutes",
+		duration.Minutes())
+	d := duration
+	defer j.wg.Done()
+	for {
+		select {
+		case <-time.After(d):
+			j.log.Debugf("start hashing")
+			j.proofer.startHashing()
+			now := time.Now()
+			intf := j.calendar.pickNextStartEvent(now)
+
+			if nil == intf {
+				return
+			}
+
+			j.calendar.rescheduleStartEventsPrior(now)
+			nextEvent := intf.(time.Time)
+			d = j.timeDurationFromSrc2Dest(now, nextEvent)
+			j.log.Infof("next start event at %s, duration: %.1f minutes",
+				nextEvent.String(), d.Minutes())
+		case <-j.channels.startEventChannel:
+			j.log.Debug("reset event, terminate start goroutine...")
+			return
+		}
+	}
+	j.log.Infof("start event terminated...")
+}
+
+func (j *JobManagerData) waitNextHasingStopEvent(duration time.Duration) {
+	j.log.Infof("create goroutine for stop events, next event duration: %.1f minutes",
+		duration.Minutes())
+	d := duration
+	defer j.wg.Done()
+	for {
+		select {
+		case <-time.After(d):
+			j.log.Debug("stop hashing")
+			j.proofer.stopHashing()
+			now := time.Now()
+			intf := j.calendar.pickNextStopEvent(now)
+
+			if nil == intf {
+				return
+			}
+
+			j.calendar.rescheduleStopEventsPrior(now)
+			nextEvent := intf.(time.Time)
+			d = j.timeDurationFromSrc2Dest(now, nextEvent)
+			j.log.Infof("next stop event: %s, duration: %.1f minutes",
+				nextEvent.String(), d.Minutes())
+		case <-j.channels.stopEventChannel:
+			j.log.Debug("reset event, terminate stop goroutine...")
+			return
+		}
+	}
+	j.log.Infof("stop event terminated...")
+}
+
+func (j *JobManagerData) resetAllEvent() {
+	j.log.Infof("reset all events...")
+	j.wg.Add(2)
+	j.channels.startEventChannel <- struct{}{}
+	j.channels.stopEventChannel <- struct{}{}
+	j.wg.Wait()
+	return
+}
+
+func (j *JobManagerData) Start() {
+	go j.waitForRefresh()
+}
+
+func (j *JobManagerData) reschedule() {
+	j.log.Debug("reschedule...")
+	j.rescheduleStartEvent()
+	j.rescheduleStopEvent()
+}
+
+func (j *JobManagerData) rescheduleStartEvent() {
+	now := time.Now()
+	intf := j.calendar.pickInitialiseStartEvent(now)
+	nextEvent := intf.(time.Time)
+	duration := nextEvent.Sub(now)
+	go j.waitNextHasingStartEvent(duration)
+}
+
+func (j *JobManagerData) rescheduleStopEvent() {
+	now := time.Now()
+	intf := j.calendar.pickInitialiseStopEvent(now)
+	if nil == intf {
+		return
+	}
+	nextEvent := intf.(time.Time)
+	duration := nextEvent.Sub(now)
+	go j.waitNextHasingStopEvent(duration)
+}
+
+func (j *JobManagerData) timeDurationFromSrc2Dest(src time.Time, dest time.Time) time.Duration {
+	d := dest.Sub(src)
+	j.log.Debugf("source time %s, destination time: %s, duration %.1f minutes",
+		src.String(), dest.String(), d.Minutes(),
+	)
+	return d
+}
---
+++ command/recorderd/job_manager_test.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/bitmark-inc/logger"
+)
+
+const (
+	defaultDelay     = time.Duration(1) * time.Microsecond
+	defaultCheckTime = time.Duration(1) * time.Millisecond
+)
+
+var channels JobManagerChannel
+var hashingCalled bool
+var managerLogging logger.Configuration
+
+type FakeProofer struct{}
+
+func (p *FakeProofer) startHashing() {
+	hashingCalled = true
+}
+
+func (p *FakeProofer) stopHashing() {
+	hashingCalled = true
+}
+
+func (p *FakeProofer) refresh() {}
+
+func (p *FakeProofer) isWorking() bool {
+	return true
+}
+
+type FakeCalendar struct{}
+
+func (c *FakeCalendar) pickNextStartEvent(t time.Time) interface{} {
+	return time.Now()
+}
+func (c *FakeCalendar) pickNextStopEvent(t time.Time) interface{} {
+	return time.Now()
+}
+func (c *FakeCalendar) pickInitialiseStartEvent(time.Time) interface{} {
+	return time.Now()
+}
+func (c *FakeCalendar) pickInitialiseStopEvent(time.Time) interface{} {
+	return time.Now()
+}
+func (c *FakeCalendar) refresh(ConfigCalendar)               {}
+func (c *FakeCalendar) rescheduleStartEventsPrior(time.Time) {}
+func (c *FakeCalendar) rescheduleStopEventsPrior(time.Time)  {}
+func (c *FakeCalendar) setLog(*logger.L)                     {}
+
+func setupTestJobManager() *JobManagerData {
+	p := setupProoferInterface()
+	channels.rescheduleChannel = make(chan struct{})
+	channels.startEventChannel = make(chan struct{}, 1)
+	channels.stopEventChannel = make(chan struct{}, 1)
+	setupTestManagerLogger()
+
+	j := &JobManagerData{
+		calendar: &FakeCalendar{},
+		proofer:  p,
+		channels: JobManagerChannel{
+			rescheduleChannel: channels.rescheduleChannel,
+			startEventChannel: channels.startEventChannel,
+			stopEventChannel:  channels.stopEventChannel,
+		},
+		log: logger.New("test"),
+	}
+	return j
+}
+
+func setupTestManagerLogger() {
+	_ = os.Mkdir(logDirectory, 0770)
+	managerLogging = loggerConfiguration()
+	_ = logger.Initialise(managerLogging)
+}
+
+func teardownManager() {
+	logger.Finalise()
+	removeLogFiles()
+}
+
+func setupProoferInterface() Proofer {
+	p := &FakeProofer{}
+	return p
+}
+
+func TestWaitNextHashingStartEvent(t *testing.T) {
+	j := setupTestJobManager()
+	defer teardownManager()
+	hashingCalled = false
+	j.wg.Add(1)
+	go j.waitNextHasingStartEvent(defaultDelay)
+	time.Sleep(defaultCheckTime)
+	j.channels.startEventChannel <- struct{}{}
+	j.wg.Wait()
+
+	if !hashingCalled {
+		t.Errorf("proofer start hashing is not called.")
+	}
+}
+
+func TestWaitNextHashingStopEvent(t *testing.T) {
+	j := setupTestJobManager()
+	defer teardownManager()
+	hashingCalled = false
+	j.wg.Add(1)
+	go j.waitNextHasingStopEvent(defaultDelay)
+	time.Sleep(defaultCheckTime)
+	j.channels.stopEventChannel <- struct{}{}
+	j.wg.Wait()
+
+	if !hashingCalled {
+		t.Errorf("proofer start hashing is not called.")
+	}
+}
+
+func TestResetAllEvent(t *testing.T) {
+	j := setupTestJobManager()
+	defer teardownManager()
+
+	received := 0
+	goRoutinCount := 2
+	var wg sync.WaitGroup
+	wg.Add(goRoutinCount)
+	go func() {
+		for i := 0; i < goRoutinCount; i++ {
+			select {
+			case <-j.channels.startEventChannel:
+				received++
+				wg.Done()
+			case <-j.channels.stopEventChannel:
+				received++
+				wg.Done()
+			}
+
+		}
+	}()
+	go j.resetAllEvent()
+	wg.Wait()
+
+	if received != goRoutinCount {
+		t.Errorf("reset all event signal is not received")
+	}
+}
+
+func TestTimeDurationFromSrc2Dest(t *testing.T) {
+	j := setupTestJobManager()
+	defer teardownManager()
+
+	now := time.Now()
+	fixture := []struct {
+		src      time.Time
+		expected time.Duration
+	}{
+		{now, time.Duration(10) * time.Minute},
+		{now, time.Duration(100) * time.Hour},
+	}
+
+	for i, s := range fixture {
+		actual := j.timeDurationFromSrc2Dest(now, now.Add(s.expected))
+		if actual != s.expected {
+			t.Errorf("%dth test fail, wrong time duration", i)
+			t.Errorf("duration of %s to %s",
+				stringifyTime(now), stringifyTime(now.Add(s.expected)))
+			t.Errorf("expect %s but get %s", s.expected, actual)
+		}
+	}
+}
