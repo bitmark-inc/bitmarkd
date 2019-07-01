bb51968477320812227e7c4aa79476e626d2569f
Aaron Chang <xesuschang@gmail.com>
2019-07-01 15:39:33 +0800

    [blockrecord] fix typos

--- blockrecord/validator_test.go
+++ blockrecord/validator_test.go
@@ -16,71 +16,71 @@ import (
 )
 
 func TestValidBlockTimeSpacingWhenInitialVersionValid(t *testing.T) {
-	actual := blockrecord.ValidBlockTimeSpacingAtVersion(1, 10)
-	assert.Equal(t, nil, actual, "faile to validate valid v1 blcok time spacing")
+	err := blockrecord.ValidBlockTimeSpacingAtVersion(1, 10)
+	assert.Equal(t, nil, err, "valid initial block time spacing")
 }
 
 func TestValidBlockTimeSpacingWhenCurrentVersionValid(t *testing.T) {
-	actual := blockrecord.ValidBlockTimeSpacingAtVersion(2, uint64(100))
-	assert.Equal(t, nil, actual, "faile to validate valid v2 blcok time spacing")
+	err := blockrecord.ValidBlockTimeSpacingAtVersion(2, uint64(100))
+	assert.Equal(t, nil, err, "valid current block time spacing")
 }
 
 func TestValidBlockTimeSpacingWhenInitialVersionInvalid(t *testing.T) {
-	actual := blockrecord.ValidBlockTimeSpacingAtVersion(1, uint64(100000))
-	assert.Equal(t, fault.ErrInvalidBlockHeaderTimestamp, actual, "faile to validate invalid v1 blcok time spacing")
+	err := blockrecord.ValidBlockTimeSpacingAtVersion(1, uint64(100000))
+	assert.Equal(t, fault.ErrInvalidBlockHeaderTimestamp, err, "invalid initial block time spacing")
 }
 
 func TestValidBlockTimeSpacingWhenCurrentVersionInvalid(t *testing.T) {
-	actual := blockrecord.ValidBlockTimeSpacingAtVersion(2, uint64(10000))
-	assert.Equal(t, fault.ErrInvalidBlockHeaderTimestamp, actual, "faile to validate invalid v2 blcok time spacing")
+	err := blockrecord.ValidBlockTimeSpacingAtVersion(2, uint64(10000))
+	assert.Equal(t, fault.ErrInvalidBlockHeaderTimestamp, err, "invalid current block time spacing")
 }
 
 func TestValidIncomingDifficutyWhenValid(t *testing.T) {
 	difficulty.Current.Set(2)
 	incoming := difficulty.New()
 	incoming.Set(2)
-	actual := blockrecord.ValidIncomingDifficuty(incoming)
+	err := blockrecord.ValidIncomingDifficuty(incoming)
 
-	assert.Equal(t, nil, actual, "fail to validate valid incoming difficulty")
+	assert.Equal(t, nil, err, "valid incoming difficulty")
 }
 
 func TestValidIncomingDifficutyWhenInValid(t *testing.T) {
 	difficulty.Current.Set(2)
 	incoming := difficulty.New()
 	incoming.Set(4)
-	actual := blockrecord.ValidIncomingDifficuty(incoming)
+	err := blockrecord.ValidIncomingDifficuty(incoming)
 
-	assert.Equal(t, fault.ErrDifficultyNotMatch, actual, "fail to validate invalid incoming difficulty")
+	assert.Equal(t, fault.ErrDifficultyNotMatch, err, "invalid incoming difficulty")
 }
 
 func TestIsDifficultyAppliedVersionWhenApplied(t *testing.T) {
-	actual := blockrecord.IsDifficultyAppliedVersion(3)
-	assert.Equal(t, true, actual, "fail to check difficulty applied version")
+	ok := blockrecord.IsDifficultyAppliedVersion(3)
+	assert.Equal(t, true, ok, "difficulty applied version")
 }
 
 func TestIsDifficultyAppliedVersionWhenNotApplied(t *testing.T) {
-	actual := blockrecord.IsDifficultyAppliedVersion(1)
-	assert.Equal(t, false, actual, "fail to check difficulty not applied version")
+	ok := blockrecord.IsDifficultyAppliedVersion(1)
+	assert.Equal(t, false, ok, "difficulty not applied version")
 }
 
 func TestValidHeaderVersionWhenTooSmall(t *testing.T) {
-	actual := blockrecord.ValidHeaderVersion(uint16(10), uint16(0))
-	assert.Equal(t, fault.ErrInvalidBlockHeaderVersion, actual, "fail to validate header version too small")
+	err := blockrecord.ValidHeaderVersion(uint16(10), uint16(0))
+	assert.Equal(t, fault.ErrInvalidBlockHeaderVersion, err, "header version small")
 }
 
 func TestValidHeaderVersionWhenPreviousLarger(t *testing.T) {
-	actual := blockrecord.ValidHeaderVersion(uint16(100), uint16(99))
-	assert.Equal(t, fault.ErrBlockVersionMustNotDecrease, actual, "fail to validate previous version larger")
+	err := blockrecord.ValidHeaderVersion(uint16(100), uint16(99))
+	assert.Equal(t, fault.ErrBlockVersionMustNotDecrease, err, "previous header version larger")
 }
 
 func TestValidHeaderVersionWhenIncomingLarger(t *testing.T) {
-	actual := blockrecord.ValidHeaderVersion(uint16(100), uint16(101))
-	assert.Equal(t, nil, actual, "fail to validate incoming version larger")
+	err := blockrecord.ValidHeaderVersion(uint16(100), uint16(101))
+	assert.Equal(t, nil, err, "incoming header version larger")
 }
 
 func TestValidHeaderVersionWhenIncomingSame(t *testing.T) {
-	actual := blockrecord.ValidHeaderVersion(uint16(100), uint16(100))
-	assert.Equal(t, nil, actual, "fail to validate incoming version same")
+	err := blockrecord.ValidHeaderVersion(uint16(100), uint16(100))
+	assert.Equal(t, nil, err, "incoming header version same")
 }
 
 func TestValidBlockLinkageWhenInvalid(t *testing.T) {
@@ -98,8 +98,8 @@ func TestValidBlockLinkageWhenInvalid(t *testing.T) {
 		0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
 	}
 
-	actual := blockrecord.ValidBlockLinkage(current, incoming)
-	assert.Equal(t, fault.ErrPreviousBlockDigestDoesNotMatch, actual, "failt to validate different digest")
+	err := blockrecord.ValidBlockLinkage(current, incoming)
+	assert.Equal(t, fault.ErrPreviousBlockDigestDoesNotMatch, err, "incoming digest different")
 }
 
 func TestValidBlockLinkageWhenValid(t *testing.T) {
@@ -117,6 +117,6 @@ func TestValidBlockLinkageWhenValid(t *testing.T) {
 		0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff,
 	}
 
-	actual := blockrecord.ValidBlockLinkage(current, incoming)
-	assert.Equal(t, nil, actual, "failt to validate same digest")
+	err := blockrecord.ValidBlockLinkage(current, incoming)
+	assert.Equal(t, nil, err, "incoming digest same")
 }
