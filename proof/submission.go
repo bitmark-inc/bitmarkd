cd034b6d8d5d061107d568d23687292f7fa197c8
Aaron Chang <aaron@bitmark.com>
2019-11-18 17:52:55 +0800

    [proof] remove redundant code

--- proof/submission.go
+++ proof/submission.go
@@ -17,8 +17,7 @@ import (
 )
 
 const (
-	submissionZapDomain = "submit"
-	submissionSignal    = "inproc://bitmark-submission-signal"
+	submissionSignal = "inproc://bitmark-submission-signal"
 )
 
 type submission struct {
@@ -55,16 +54,6 @@ func (sub *submission) initialise(configuration *Configuration) error {
 		return err
 	}
 
-	// create connections
-	//c, _ := util.NewConnections(configuration.Submit)
-
-	// allocate IPv4 and IPv6 sockets
-	//sub.socket4, sub.socket6, err = zmqutil.NewBind(log, zmq.REP, submissionZapDomain, privateKey, publicKey, c)
-	//if nil != err {
-	//	log.Errorf("bind error: %s", err)
-	//	return err
-	//}
-
 	return nil
 }
 
@@ -77,98 +66,20 @@ func (sub *submission) Run(args interface{}, shutdown <-chan struct{}) {
 	log.Info("starting…")
 
 	go func() {
-		//poller := zmq.NewPoller()
-		//if nil != sub.socket4 {
-		//	poller.Add(sub.socket4, zmq.POLLIN)
-		//}
-		//if nil != sub.socket6 {
-		//	poller.Add(sub.socket6, zmq.POLLIN)
-		//}
-		//poller.Add(sub.sigReceive, zmq.POLLIN)
-		//loop:
-		//	for {
-		//		log.Debug("waiting…")
-		//		sockets, _ := poller.Poll(-1)
-		//		for _, socket := range sockets {
-		//			switch s := socket.Socket; s {
-		//			case sub.sigReceive:
-		//				s.Recv(0)
-		//				break loop
-		//			default:
-		//				sub.process(s)
-		//			}
-		//		}
-		//	}
-		//	sub.sigReceive.Close()
-		//if nil != sub.socket4 {
-		//	sub.socket4.Close()
-		//}
-		//if nil != sub.socket6 {
-		//	sub.socket6.Close()
-		//}
-
-		for j := range possibleHashCh {
+		for hash := range possibleHashCh {
 			log.Debug("receive possible hash")
-			sub.processP2P(j)
+			sub.process(hash)
 		}
 	}()
 
 	// wait for shutdown
 	<-shutdown
-	sub.sigSend.SendMessage("stop")
-	sub.sigSend.Close()
+	_, _ = sub.sigSend.SendMessage("stop")
+	_ = sub.sigSend.Close()
 }
 
-// process the request and return response to prooferd
-func (sub *submission) process(socket *zmq.Socket) {
-	log := sub.log
-
-	data, err := socket.RecvMessage(0)
-	if nil != err {
-		log.Errorf("JSON encode error: %s", err)
-		return
-	}
-
-	log.Infof("received message: %q", data)
-
-	ok := false
-	var request SubmittedItem
-	err = json.Unmarshal([]byte(data[0]), &request)
-	if nil != err {
-		log.Errorf("JSON decode error: %s", err)
-	} else {
-
-		log.Infof("received message: %v", request)
-
-		ok = matchToJobQueue(&request, log)
-
-		log.Infof("matches: %v", ok)
-	}
-
-	response := struct {
-		Job string `json:"job"`
-		OK  bool   `json:"ok"`
-	}{
-		Job: request.Job,
-		OK:  ok,
-	}
-
-	result, err := json.Marshal(response)
-	logger.PanicIfError("JSON encode error", err)
-
-	log.Infof("json to send: %s", result)
-
-	// if _, err := socket.Send(to, zmq.SNDMORE|zmq.DONTWAIT); nil != err {
-	// 	return err
-	// }
-	// if _, err := socket.Send(command, zmq.SNDMORE|zmq.DONTWAIT); nil != err {
-	// 	return err
-	// }
-	_, err = socket.SendBytes(result, 0|zmq.DONTWAIT)
-	logger.PanicIfError("Submission", err)
-}
-
-func (sub *submission) processP2P(data []byte) {
+// process the request and return response to recorderd
+func (sub *submission) process(data []byte) {
 	log := sub.log
 
 	log.Infof("received message: %q", data)
