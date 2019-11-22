e31ed0badb6bf5580fb8dc040958d7a88378d734
Jim Yeh <jim@bitmark.com>
2019-11-22 14:08:36 +0800

    [ci] improve the security of digitalocean freebsd image

--- .circleci/packer/packer_do_freebsd.sh
+++ .circleci/packer/packer_do_freebsd.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-if [ "$PACKER_BUILDER_TYPE" != "digitalocean" ]
-then
-    return 0;
-fi
-
 sudo pkg install --yes pkgconf-1.6.3,1 git go
 sudo pkg install --yes libargon2-20190702 libzmq4-4.3.1_1
 
@@ -18,16 +13,22 @@ cd bitmark-wallet && git checkout v0.6.3 && mkdir bin
 go build -o bin -ldflags "-X main.version=0.6.3" ./...
 cd
 
-su root -c "mv bitmarkd/bin/bitmarkd /usr/local/sbin/"
-su root -c "mv bitmarkd/bin/recorderd /usr/local/sbin/"
+sudo mv bitmarkd/bin/bitmarkd /usr/local/sbin/
+sudo mv bitmarkd/bin/recorderd /usr/local/sbin/
 
-su root -c "mv bitmarkd/bin/* /usr/local/bin/"
-su root -c "mv bitmark-wallet/bin/* /usr/local/bin/"
+sudo mv bitmarkd/bin/* /usr/local/bin/
+sudo mv bitmark-wallet/bin/* /usr/local/bin/
 
-sudo rm -rf $HOME/go $HOME/bitmarkd
+rm -rf go bitmarkd
+rm -f .ssh/authorized_keys
+
+sudo rm -f /etc/ssh/*key*
 sudo pkg remove -y perl5 go
 sudo pkg clean -ay
 
-# https://github.com/terraform-providers/terraform-provider-digitalocean/issues/243#issuecomment-508226846
-cat /dev/null > $HOME/.ssh/authorized_keys
+sudo rm -rf /tmp/* /var/tmp/*
+sudo rm -rf /var/lib/cloud/instances/*
 sudo rm -rf /var/lib/cloud/instance
+
+sudo rm -rf /var/log/*.gz /var/log/*.[0-9] /var/log/*-???????? /var/log/*.log /var/log/utx.*
+sudo dd if=/dev/zero of=/zerofile && sync; sudo rm /zerofile; sync
