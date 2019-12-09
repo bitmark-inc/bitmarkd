efb6dd5155f9f2627a79a3b4c8936da59ebf62cd
Jim Yeh <lemonlatte@gmail.com>
2019-12-09 09:55:34 +0800

    [payment,storage,currency,command] make p2p validation supporting regtest payment blockchains (#103)
    * fix and add new test cases

---
+++ currency/chain_test.go
@@ -0,0 +1,73 @@
+package currency
+
+import (
+	"testing"
+
+	"github.com/bitmark-inc/bitmarkd/chain"
+)
+
+func TestBtcChainParams(t *testing.T) {
+
+	btcLocalParams := Bitcoin.ChainParam(chain.Local)
+
+	if "regtest" != btcLocalParams.Name {
+		t.Fatal("invalid network")
+	}
+
+	if 1 != btcLocalParams.HDCoinType {
+		t.Fatal("incorrect currency")
+	}
+
+	btcTestnetParams := Bitcoin.ChainParam(chain.Testing)
+
+	if "testnet3" != btcTestnetParams.Name {
+		t.Fatal("invalid network")
+	}
+
+	if 1 != btcTestnetParams.HDCoinType {
+		t.Fatal("incorrect currency")
+	}
+
+	btcMainnetParams := Bitcoin.ChainParam(chain.Bitmark)
+
+	if "mainnet" != btcMainnetParams.Name {
+		t.Fatal("invalid network")
+	}
+
+	if 0 != btcMainnetParams.HDCoinType {
+		t.Fatal("incorrect currency")
+	}
+}
+
+func TestLtcChainParams(t *testing.T) {
+
+	ltcLocalParams := Litecoin.ChainParam(chain.Local)
+
+	if "regtest" != ltcLocalParams.Name {
+		t.Fatal("invalid network")
+	}
+
+	if 1 != ltcLocalParams.HDCoinType {
+		t.Fatal("incorrect currency")
+	}
+
+	ltcTestnetParams := Litecoin.ChainParam(chain.Testing)
+
+	if "testnet4" != ltcTestnetParams.Name {
+		t.Fatal("invalid network")
+	}
+
+	if 1 != ltcTestnetParams.HDCoinType {
+		t.Fatal("incorrect currency")
+	}
+
+	ltcMainnetParams := Litecoin.ChainParam(chain.Bitmark)
+
+	if "mainnet" != ltcMainnetParams.Name {
+		t.Fatal("invalid network")
+	}
+
+	if 2 != ltcMainnetParams.HDCoinType {
+		t.Fatal("incorrect currency")
+	}
+}
--- payment/setup.go
+++ payment/setup.go
@@ -27,10 +27,16 @@ const (
 
 // Configuration - structure for configuration file
 type Configuration struct {
-	Mode      string                  `gluamapper:"mode" hcl:"mode" json:"mode"`
-	Discovery *discoveryConfiguration `gluamapper:"discovery" hcl:"discovery" json:"discovery"`
-	Bitcoin   *currencyConfiguration  `gluamapper:"bitcoin" hcl:"bitcoin" json:"bitcoin"`
-	Litecoin  *currencyConfiguration  `gluamapper:"litecoin" hcl:"litecoin" json:"litecoin"`
+	Mode           string                      `gluamapper:"mode" hcl:"mode" json:"mode"`
+	BootstrapNodes bootstrapNodesConfiguration `gluamapper:"bootstrap_nodes" hcl:"bootstrap_nodes" json:"bootstrap_nodes"`
+	Discovery      *discoveryConfiguration     `gluamapper:"discovery" hcl:"discovery" json:"discovery"`
+	Bitcoin        *currencyConfiguration      `gluamapper:"bitcoin" hcl:"bitcoin" json:"bitcoin"`
+	Litecoin       *currencyConfiguration      `gluamapper:"litecoin" hcl:"litecoin" json:"litecoin"`
+}
+
+type bootstrapNodesConfiguration struct {
+	Bitcoin  []string `gluamapper:"bitcoin" hcl:"bitcoin" json:"bitcoin"`
+	Litecoin []string `gluamapper:"litecoin" hcl:"litecoin" json:"litecoin"`
 }
 
 type discoveryConfiguration struct {
@@ -107,11 +113,11 @@ func Initialise(configuration *Configuration) error {
 	case "p2p":
 		globalData.log.Info("p2p watcher…")
 
-		btcP2pWatcher, err := newP2pWatcher(currency.Bitcoin)
+		btcP2pWatcher, err := newP2pWatcher(currency.Bitcoin, configuration.BootstrapNodes.Bitcoin)
 		if err != nil {
 			return err
 		}
-		ltcP2pWatcher, err := newP2pWatcher(currency.Litecoin)
+		ltcP2pWatcher, err := newP2pWatcher(currency.Litecoin, configuration.BootstrapNodes.Litecoin)
 		if err != nil {
 			return err
 		}
