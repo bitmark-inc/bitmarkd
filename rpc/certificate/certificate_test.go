57ddb9e8d55ceb631938e85321c7187322a058ca
Aaron Chang <aaron@bitmark.com>
2020-03-04 14:05:14 +0800

    [rpc] add unit test to certificate

--- rpc/certificate/certificate_test.go
+++ rpc/certificate/certificate_test.go
@@ -4,3 +4,42 @@
 // license that can be found in the LICENSE file.
 
 package certificate_test
+
+import (
+	"crypto/tls"
+	"os"
+	"path"
+	"path/filepath"
+	"testing"
+
+	"golang.org/x/crypto/sha3"
+
+	"github.com/bitmark-inc/bitmarkd/rpc/certificate"
+	"github.com/bitmark-inc/logger"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/bitmark-inc/bitmarkd/rpc/fixtures"
+)
+
+func TestGet(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	wd, _ := os.Getwd()
+	fixtureDir := path.Join(filepath.Dir(wd), "fixtures")
+	cer := fixtures.Certificate(fixtureDir)
+	key := fixtures.Key(fixtureDir)
+
+	tlsConfig, fingerprint, err := certificate.Get(
+		logger.New(fixtures.LogCategory),
+		"test",
+		cer,
+		key,
+	)
+	assert.Nil(t, err, "wrong Get")
+
+	pair, _ := tls.X509KeyPair([]byte(cer), []byte(key))
+
+	assert.Equal(t, sha3.Sum256(pair.Certificate[0]), fingerprint, "wrong fingerprint")
+	assert.Equal(t, pair, tlsConfig.Certificates[0], "wrong config")
+}
