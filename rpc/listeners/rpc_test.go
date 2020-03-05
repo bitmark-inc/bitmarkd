7f5640fda86b25c0939f063ac0d3ac9fac8efbbe
Aaron Chang <xesuschang@gmail.com>
2020-03-05 09:21:19 +0800

    [rpc] add unit test to listener/rpc

--- rpc/listeners/rpc_test.go
+++ rpc/listeners/rpc_test.go
@@ -16,6 +16,8 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/bitmark-inc/bitmarkd/fault"
+
 	"github.com/bitmark-inc/bitmarkd/rpc/certificate"
 
 	"github.com/stretchr/testify/assert"
@@ -113,4 +115,322 @@ func TestRpcListenerServe(t *testing.T) {
 	err = client.Call("Add.Add", &arg, &reply)
 	assert.Nil(t, err, "wrong client Call")
 	assert.Equal(t, arg.A+arg.B, reply, "wrong result")
+}
+
+func TestRpcListenerServeWhenMaxConnectionCountTooSmall(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	port := rand.Intn(30000) + 30000
+	listen := fmt.Sprintf("127.0.0.1:%d", port)
+	con := listeners.RPCConfiguration{
+		MaximumConnections: 0,
+		Bandwidth:          10000000,
+		Listen:             []string{listen},
+		Certificate:        "",
+		Announce:           []string{"127.0.0.1:9999"},
+	}
+
+	count := counter.Counter(0)
+
+	s := rpc.NewServer()
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	a := mocks.NewMockAnnounce(ctl)
+
+	_, err := listeners.NewRPC(
+		&con,
+		logger.New(fixtures.LogCategory),
+		&count,
+		s,
+		a,
+		&tls.Config{},
+		[32]byte{},
+	)
+	assert.Equal(t, fault.MissingParameters, err, "wrong error")
+}
+
+func TestRpcListenerServeWhenBandwidthTooSmall(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	port := rand.Intn(30000) + 30000
+	listen := fmt.Sprintf("127.0.0.1:%d", port)
+	con := listeners.RPCConfiguration{
+		MaximumConnections: 1,
+		Bandwidth:          100,
+		Listen:             []string{listen},
+		Certificate:        "",
+		Announce:           []string{"127.0.0.1:9999"},
+	}
+
+	count := counter.Counter(0)
+
+	s := rpc.NewServer()
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	a := mocks.NewMockAnnounce(ctl)
+
+	_, err := listeners.NewRPC(
+		&con,
+		logger.New(fixtures.LogCategory),
+		&count,
+		s,
+		a,
+		&tls.Config{},
+		[32]byte{},
+	)
+	assert.Equal(t, fault.MissingParameters, err, "wrong error")
+}
+
+func TestRpcListenerServeWhenEmptyListen(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	con := listeners.RPCConfiguration{
+		MaximumConnections: 1,
+		Bandwidth:          10000000,
+		Listen:             []string{},
+		Certificate:        "",
+		Announce:           []string{"127.0.0.1:9999"},
+	}
+
+	count := counter.Counter(0)
+
+	s := rpc.NewServer()
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	a := mocks.NewMockAnnounce(ctl)
+
+	_, err := listeners.NewRPC(
+		&con,
+		logger.New(fixtures.LogCategory),
+		&count,
+		s,
+		a,
+		&tls.Config{},
+		[32]byte{},
+	)
+	assert.Equal(t, fault.MissingParameters, err, "wrong error")
+}
+
+func TestRpcListenerServeWhenWrongAnnounce(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	port := rand.Intn(30000) + 30000
+	listen := fmt.Sprintf("127.0.0.1:%d", port)
+	con := listeners.RPCConfiguration{
+		MaximumConnections: 1,
+		Bandwidth:          10000000,
+		Listen:             []string{listen},
+		Certificate:        "",
+		Announce:           []string{"", "1"},
+	}
+
+	count := counter.Counter(0)
+
+	s := rpc.NewServer()
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	a := mocks.NewMockAnnounce(ctl)
+
+	_, err := listeners.NewRPC(
+		&con,
+		logger.New(fixtures.LogCategory),
+		&count,
+		s,
+		a,
+		&tls.Config{},
+		[32]byte{},
+	)
+	assert.NotNil(t, err, "wrong error")
+}
+
+func TestRpcListenerServeWhenErrorAnnounceSet(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	port := rand.Intn(30000) + 30000
+	listen := fmt.Sprintf("127.0.0.1:%d", port)
+	con := listeners.RPCConfiguration{
+		MaximumConnections: 5,
+		Bandwidth:          10000000,
+		Listen:             []string{listen},
+		Certificate:        "",
+		Announce:           []string{"127.0.0.1:9999"},
+	}
+
+	count := counter.Counter(0)
+
+	s := rpc.NewServer()
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	a := mocks.NewMockAnnounce(ctl)
+	a.EXPECT().Set(gomock.Any(), gomock.Any()).Return(fmt.Errorf("fake error")).Times(1)
+
+	_, err := listeners.NewRPC(
+		&con,
+		logger.New(fixtures.LogCategory),
+		&count,
+		s,
+		a,
+		&tls.Config{},
+		[32]byte{},
+	)
+	assert.NotNil(t, err, "wrong error")
+	assert.Equal(t, "fake error", err.Error(), "wrong error message")
+}
+
+func TestRpcListenerServeWhenErrorListen(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	con := listeners.RPCConfiguration{
+		MaximumConnections: 5,
+		Bandwidth:          10000000,
+		Listen:             []string{"1"},
+		Certificate:        "",
+		Announce:           []string{"127.0.0.1:9999"},
+	}
+
+	count := counter.Counter(0)
+
+	s := rpc.NewServer()
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	a := mocks.NewMockAnnounce(ctl)
+	a.EXPECT().Set(gomock.Any(), gomock.Any()).Return(nil).Times(1)
+
+	_, err := listeners.NewRPC(
+		&con,
+		logger.New(fixtures.LogCategory),
+		&count,
+		s,
+		a,
+		&tls.Config{},
+		[32]byte{},
+	)
+	assert.NotNil(t, err, "wrong error")
+	assert.Equal(t, fault.InvalidIpAddress, err, "wrong error message")
+}
+
+func TestRpcListenerServeWhenListenAll(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	con := listeners.RPCConfiguration{
+		MaximumConnections: 5,
+		Bandwidth:          10000000,
+		Listen:             []string{"*:1234"},
+		Certificate:        "",
+		Announce:           []string{"127.0.0.1:9999"},
+	}
+
+	count := counter.Counter(0)
+
+	s := rpc.NewServer()
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	a := mocks.NewMockAnnounce(ctl)
+	a.EXPECT().Set(gomock.Any(), gomock.Any()).Return(nil).Times(1)
+
+	_, err := listeners.NewRPC(
+		&con,
+		logger.New(fixtures.LogCategory),
+		&count,
+		s,
+		a,
+		&tls.Config{},
+		[32]byte{},
+	)
+	assert.Nil(t, err, "wrong NewRPC")
+}
+
+func TestRpcListenerServeWhenListenIPv6(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	con := listeners.RPCConfiguration{
+		MaximumConnections: 5,
+		Bandwidth:          10000000,
+		Listen:             []string{"[1:2:3:4:5:6:7:8]::1"},
+		Certificate:        "",
+		Announce:           []string{"127.0.0.1:9999"},
+	}
+
+	count := counter.Counter(0)
+
+	s := rpc.NewServer()
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	a := mocks.NewMockAnnounce(ctl)
+	a.EXPECT().Set(gomock.Any(), gomock.Any()).Return(nil).Times(1)
+
+	_, err := listeners.NewRPC(
+		&con,
+		logger.New(fixtures.LogCategory),
+		&count,
+		s,
+		a,
+		&tls.Config{},
+		[32]byte{},
+	)
+	assert.Nil(t, err, "wrong NewRPC")
+}
+
+func TestRpcListenerServeWhenInvalidTLSConfig(t *testing.T) {
+	fixtures.SetupTestLogger()
+	defer fixtures.TeardownTestLogger()
+
+	port := rand.Intn(30000) + 30000
+	listen := fmt.Sprintf("127.0.0.1:%d", port)
+	con := listeners.RPCConfiguration{
+		MaximumConnections: 5,
+		Bandwidth:          10000000,
+		Listen:             []string{listen},
+		Certificate:        "",
+		Announce:           []string{"127.0.0.1:9999"},
+	}
+
+	count := counter.Counter(0)
+
+	s := rpc.NewServer()
+
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	a := mocks.NewMockAnnounce(ctl)
+	a.EXPECT().Set(gomock.Any(), gomock.Any()).Return(nil).Times(1)
+
+	l, err := listeners.NewRPC(
+		&con,
+		logger.New(fixtures.LogCategory),
+		&count,
+		s,
+		a,
+		&tls.Config{},
+		[32]byte{},
+	)
+	assert.Nil(t, err, "wrong NewRPC")
+
+	err = l.Serve()
+	assert.NotNil(t, err, "wrong Serve")
+	assert.Contains(t, err.Error(), "tls", "wrong error message")
 }
