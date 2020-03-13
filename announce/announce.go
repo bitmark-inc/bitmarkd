30e61ce3130157435453b06bd98147147290758b
Aaron Chang <xesuschang@gmail.com>
2020-03-13 11:11:39 +0800

    [announce] add Announce interface

---
+++ announce/announce.go
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: ISC
+// Copyright (c) 2014-2020 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package announce
+
+import (
+	"github.com/bitmark-inc/bitmarkd/announce/fingerprint"
+	"github.com/bitmark-inc/bitmarkd/announce/rpc"
+)
+
+type Announce interface {
+	// Set - set this node's rpc announcement data
+	Set(fingerprint.Fingerprint, []byte) error
+
+	// Fetch- fetch some records
+	Fetch(uint64, int) ([]rpc.Entry, uint64, error)
+}
+
+// Get - return Announce interface
+func Get() Announce {
+	return globalData.rpcs
+}
